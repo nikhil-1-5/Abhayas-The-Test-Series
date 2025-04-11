610feaa007ba40e696ee22cb5474298c08a3e44e
nikhil-1-5 <nikhil15augast@gmail.com>
2025-04-11 13:36:42 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('section', {
+    delay: 200,
+    distance: '50px',
+    duration: 800,
+    easing: 'ease-out',
+    origin: 'bottom'
+  });
+  
---
+++ motivation.js
@@ -0,0 +1,32 @@
+const images = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+  "images/6.jpg",
+  "images/7.jpg",
+  "images/8.jpg",
+  "images/9.jpg",
+  "images/10.jpg"
+];
+
+let index = 0;
+const imgElement = document.getElementById("motivation-img");
+
+function showImage(i) {
+  imgElement.src = images[i];
+}
+
+function nextImage() {
+  index = (index + 1) % images.length;
+  showImage(index);
+}
+
+function prevImage() {
+  index = (index - 1 + images.length) % images.length;
+  showImage(index);
+}
+function goHome() {
+  window.location.href = "home.html";
+}
